e50264cb8caf4e5ba74d71561c3b48aef9739143
anro772 <andreistefanscl@gmail.com>
2022-05-19 01:55:23 +0300

    1

---
+++ UserControlLibrary/CloseButtonControl.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UserControlLibrary
+{
+    partial class CloseButtonControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backPanel = new System.Windows.Forms.Panel();
+            this.X = new System.Windows.Forms.Label();
+            this.backPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backPanel
+            // 
+            this.backPanel.Controls.Add(this.X);
+            this.backPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backPanel.Location = new System.Drawing.Point(0, 0);
+            this.backPanel.Name = "backPanel";
+            this.backPanel.Size = new System.Drawing.Size(48, 28);
+            this.backPanel.TabIndex = 0;
+            this.backPanel.Click += new System.EventHandler(this.panel1_Click);
+            this.backPanel.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.backPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // X
+            // 
+            this.X.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.X.AutoSize = true;
+            this.X.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X.ForeColor = System.Drawing.Color.White;
+            this.X.Location = new System.Drawing.Point(8, -1);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(30, 29);
+            this.X.TabIndex = 0;
+            this.X.Text = "X";
+            this.X.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            this.X.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.X.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // CloseButtonControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.backPanel);
+            this.Name = "CloseButtonControl";
+            this.Size = new System.Drawing.Size(48, 28);
+            this.backPanel.ResumeLayout(false);
+            this.backPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel backPanel;
+        private System.Windows.Forms.Label X;
+    }
+}
---
+++ UserControlLibrary/CloseButtonControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlLibrary
+{
+    public partial class CloseButtonControl : UserControl
+    {
+        public CloseButtonControl()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            X.BackColor = Color.FromArgb(0, 129, 180);
+            backPanel.BackColor = Color.FromArgb(0, 129, 180);
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            X.BackColor = Color.FromArgb(60,60,60);
+            backPanel.BackColor = Color.FromArgb(60, 60, 60);
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
